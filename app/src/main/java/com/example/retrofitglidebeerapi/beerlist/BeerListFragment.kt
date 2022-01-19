d694ef89e6fe22a7852fa43605f860f374786c87
Andrew-Zheng <andzheng@gmail.com>
2022-01-19 15:56:48 -0500

    Implement databinding and inflate fragment views

--- app/src/main/java/com/example/retrofitglidebeerapi/beerlist/BeerListFragment.kt
+++ app/src/main/java/com/example/retrofitglidebeerapi/beerlist/BeerListFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.retrofitglidebeerapi.R
+import com.example.retrofitglidebeerapi.databinding.FragmentBeerListBinding
 
 class BeerListFragment : Fragment() {
 
@@ -14,7 +16,11 @@ class BeerListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beer_list, container, false)
+        // Get a reference to the binding object and inflate the fragment views.
+        val binding: FragmentBeerListBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_beer_list, container, false
+        )
+
+        return binding.root
     }
 }
