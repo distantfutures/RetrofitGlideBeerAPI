4478e1fb6fb51ec494c6c3d27171b13d2a865cc1
Andrew Zheng <andzheng@gmail.com>
2022-06-16 15:40:08 -0400

    RxJava test and dependencies

--- app/src/main/java/com/example/retrofitglidebeerapi/beerlist/BeerListViewModel.kt
+++ app/src/main/java/com/example/retrofitglidebeerapi/beerlist/BeerListViewModel.kt
@@ -6,12 +6,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.retrofitglidebeerapi.BeerList
 import com.example.retrofitglidebeerapi.network.BeerApi
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observable.just
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.util.*
 
 class BeerListViewModel : ViewModel() {
+    private val TAG = "VMTest"
 
     // Create Job
     private val viewModelJob = Job()
@@ -22,8 +28,14 @@ class BeerListViewModel : ViewModel() {
     val beers: LiveData<List<BeerList>>
         get() = _beers
 
+    val mList = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val aList = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
     init {
         getTheBeers()
+        justTest()
+        fromTest()
+        fromTestIter()
     }
 
     private fun getTheBeers() {
@@ -43,5 +55,72 @@ class BeerListViewModel : ViewModel() {
                 Log.i("Response", "Failed")
             }
         }
+    }
+
+    private fun justTest() {
+        val observeable = Observable.just(mList)
+
+        val observer = object : Observer<List<Int>> {
+            override fun onSubscribe(d: Disposable) {
+                Log.i(TAG, "onSubscribe")
+            }
+
+            override fun onNext(t: List<Int>) {
+                Log.i(TAG, "onNext: ${t}")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.i(TAG, "onError")
+            }
+
+            override fun onComplete() {
+                Log.i(TAG, "onComplete")
+            }
+        }
+        observeable.subscribe(observer)
+    }
+    private fun fromTest() {
+        val observeable = Observable.fromArray(aList)
+
+        val observer = object : Observer<Array<Int>> {
+            override fun onSubscribe(d: Disposable) {
+                Log.i(TAG, "onSubscribe")
+            }
+
+            override fun onNext(t: Array<Int>) {
+                Log.i(TAG, "onNext: ${Arrays.toString(t)}")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.i(TAG, "onError")
+            }
+
+            override fun onComplete() {
+                Log.i(TAG, "onComplete")
+            }
+        }
+        observeable.subscribe(observer)
+    }
+    private fun fromTestIter() {
+        val observeable = Observable.fromIterable(mList)
+
+        val observer = object : Observer<Int> {
+            override fun onSubscribe(d: Disposable) {
+                Log.i(TAG, "onSubscribe")
+            }
+
+            override fun onNext(t: Int) {
+                Log.i(TAG, "onNext: $t")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.i(TAG, "onError")
+            }
+
+            override fun onComplete() {
+                Log.i(TAG, "onComplete")
+            }
+        }
+        observeable.subscribe(observer)
     }
 }
